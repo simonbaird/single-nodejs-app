ca563ebf054f6916312d181b180f1a31935f6531
Simon Baird <simon.baird@gmail.com>
2022-01-28 15:31:30 -0500

    Whitespace fixes and delete old README content

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 const express = require("express");
-const app = express(); 
+const app = express();
 
-function run_web_site(port) {    
+function run_web_site(port) {
 	app.use(express.static('html'));
-	app.use("/", express.Router());   
+	app.use("/", express.Router());
 	app.listen(port, function () {
 		console.log("Static site hosted on port", port);
 	});
-} 
-run_web_site(8080) 
+}
+run_web_site(8080)
